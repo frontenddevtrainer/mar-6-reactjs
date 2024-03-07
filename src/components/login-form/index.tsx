58e46e23f9640e0c6419be60328999dbc9eee244
Admin <admin@example.com>
2024-03-07 15:17:17 +0000

    add additional controls

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
 const LoginForm = () => {
-  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+    remember_me: false,
+    sitename: "",
+  });
 
   const onLogin = () => {
     console.log(formState);
   };
 
   const onChangeHandler = (e) => {
-    const { name, value } = e.target;
-
-    setFormState({ ...formState, [name] : value });
+    const { name, value, checked, type } = e.target;
+    const controlValue = type === "checkbox" ? checked : value;
+    setFormState({ ...formState, [name]: controlValue });
   };
 
   return (
@@ -29,6 +34,34 @@ const LoginForm = () => {
           Password
           <input type="password" name="password" onChange={onChangeHandler} />
         </label>
+        {/* <label htmlFor="email">
+          Remember me
+          <input
+            type="checkbox"
+            name="remember_me"
+            onChange={onChangeHandler}
+          />
+        </label> */}
+        {/* <label htmlFor="email">
+          Sitename edureka.co
+          <input
+            type="radio"
+            onChange={onChangeHandler}
+            name="sitename"
+            value="edureka.co"
+          />
+          edureka.in
+          <input
+            type="radio"
+            onChange={onChangeHandler}
+            name="sitename"
+            value="edureka.in"
+          />
+          <select onChange={onChangeHandler} name="sitename">
+            <option value="edureka.co">edureka.co</option>
+            <option value="edureka.in">edureka.in</option>
+          </select>
+        </label> */}
       </p>
       <button>Submit</button>
     </form>
