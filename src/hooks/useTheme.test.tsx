53f62a42a3da658a4cfb0b3c29b37dbdd0ef648f
Admin <admin@example.com>
2024-03-15 17:55:37 +0000

    provider testing.

--- src/hooks/useTheme.test.tsx
+++ src/hooks/useTheme.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react";
+import { renderHook, act, render, screen, waitFor, fireEvent } from "@testing-library/react";
 import { useTheme } from "./useTheme";
 import { ThemeOptions, ThemeProvider } from "../context/theme";
 
@@ -8,14 +8,52 @@ describe("useTheme hook", () => {
       return <ThemeProvider>{children}</ThemeProvider>;
     };
 
-    const { result } = renderHook(() => {
-      return useTheme();
-    }, { wrapper });
+    const { result } = renderHook(
+      () => {
+        return useTheme();
+      },
+      { wrapper }
+    );
 
     expect(result.current.theme).toBe(ThemeOptions.dark);
-    act(()=>{
-        result.current.change(ThemeOptions.light);
-    })
+    act(() => {
+      result.current.change(ThemeOptions.light);
+    });
     expect(result.current.theme).toBe(ThemeOptions.light);
+  });
+
+  test("Test theme in component", async () => {
+    const TestComponent = () => {
+      const { change, theme } = useTheme();
+
+      return (
+        <ThemeProvider>
+          <div>
+            {/* Component 1 */}
+            <p>Theme: {theme}</p>
+            {/* Component 2 */}
+            <button
+              onClick={() => {
+                change(ThemeOptions.light);
+              }}
+            >
+              Click
+            </button>
+          </div>
+        </ThemeProvider>
+      );
+    };
+
+    render(<TestComponent />);
+
+    expect(screen.getByText(`Theme: ${ThemeOptions.dark}`));
+    fireEvent.click(screen.getByRole("button"));
+
+    await waitFor(()=>{
+        expect(screen.getByText(`Theme: ${ThemeOptions.light}`));
+    })
+
+    
+
   });
 });
